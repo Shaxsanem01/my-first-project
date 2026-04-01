415463932e2b6f607b6748ac4a642143acd35a5f
Shaxsanem01 <shax.jan02@gmail.com>
2026-04-01 19:11:30 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#61dafb' }}>Поздравляю! 🥳</h1>
+      <h2>Твой первый React-сайт ожил!</h2>
+      <p>Теперь мы можем делать здесь всё, что захотим.</p>
+    </div>
+  );
+}
+
+export default App;
